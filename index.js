7419b84e0a94590c51b333583279466cfaa1a713
liyah2 <aaliyahcloss@gmail.com>
2025-10-14 22:38:41 -0400

    2 bugs found and fixed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//Grabs DOM elements from HTML
 const guessInput = document.getElementById('guess');
 const submitButton = document.getElementById('submit');
 const resetButton = document.getElementById('reset');
@@ -29,16 +31,18 @@ function checkGuess() {
 
   hideAllMessages();
 
+  //Displays guess messages
   if (guess === targetNumber) {
     numberOfGuessesMessage.style.display = '';
     numberOfGuessesMessage.innerHTML = `You made ${attempts} guesses`;
 
-    correctMessage.style.display = '';
+    correctMessage.style.display = '';//putting none hid the reset button
 
     submitButton.disabled = true;
     guessInput.disabled = true;
   }
 
+  //If not equal to number, display....
   if (guess !== targetNumber) {
     if (guess < targetNumber) {
       tooLowMessage.style.display = '';
@@ -52,7 +56,8 @@ function checkGuess() {
     numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guesses remaining`;
   }
 
-  if (attempts ==== maxNumberOfAttempts) {
+  //disables button if attempts reaches the max
+  if (attempts === maxNumberOfAttempts) {
     submitButton.disabled = true;
     guessInput.disabled = true;
   }
@@ -62,19 +67,21 @@ function checkGuess() {
   resetButton.style.display = '';
 }
 
+//Hidden message - hides message
 function hideAllMessages() {
-  for (let elementIndex = 0; elementIndex <= messages.length; elementIndex++) {
-    messages[elementIndex].style.display = 'none';
+  for (let i = 0; i < messages.length; i++) {
+    messages[i].style.display = 'none';
   }
 }
 
-funtion setup() {
+//Setting game
+function setup() {
   // Get random number
   targetNumber = getRandomNumber(1, 100);
   console.log(`target number: ${targetNumber}`);
 
   // Reset number of attempts
-  maxNumberOfAttempts = 0;
+  const maxNumberOfAttempts = 0;
 
   // Enable the input and submit button
   submitButton.disabeld = false;
@@ -84,7 +91,27 @@ funtion setup() {
   resetButton.style.display = 'none';
 }
 
+//Listens to the event to know what to do next.
 submitButton.addEventListener('click', checkGuess);
 resetButton.addEventListener('click', setup);
 
+//calls it.
 setup();
+
+
+
+// ==========================Notes===============================
+
+//initially, the game doesn't respond when I input a number and click submit. The reset button also does nothing. No messages are hidden, they are all displayed.
+// Consoles an error message.
+
+
+// 1. Extra = sign
+// 2. function spelled incorrectly
+// 3. let or const missing for maxnum
+//4.change to number
+// 5. element replaced with i. Helped hide the messages and reset button
+
+
+//runs after fixing console errors, but messages still displays
+
